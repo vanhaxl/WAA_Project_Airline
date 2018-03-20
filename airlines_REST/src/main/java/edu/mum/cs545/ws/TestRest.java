fe9b11f425920dd609b9cd26d30f7d2eabff9060
Joy <oogii.joy@gmail.com>
2018-03-19 21:42:18 -0500

    TestRest

---
+++ airlines_REST/src/main/java/edu/mum/cs545/ws/TestRest.java
@@ -0,0 +1,18 @@
+package edu.mum.cs545.ws;
+
+import javax.inject.Named;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
+
+@Named
+@Path("test")
+public class TestRest {
+
+    @GET
+    public String helloWorld(@DefaultValue("Gorgeous") @QueryParam("name") String name) {
+        return "Hello REST " + name + "!";
+    }
+
+}
